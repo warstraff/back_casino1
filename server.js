bcd71c55a8c4874d3c4b3aa3e7c88b9630013872
warstraff <fntrc@proton.me>
2026-01-28 19:50:39 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 const messages = new Map(); // IP -> array of messages
 const replies = new Map(); // IP -> array of replies
